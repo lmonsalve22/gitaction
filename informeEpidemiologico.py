4529519ffba70af8f6e8a27a1d7e9b5597a696c5
Mario9191 <mariosalazar9191@gmail.com>
2020-05-26 17:57:42 -0400

    commit

--- informeEpidemiologico.py
+++ informeEpidemiologico.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 def Test():
     pd.read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/input/ReporteDiario/CamasHospital_Diario.csv").to_csv("Test.csv")
-    
-Test()
 
 if __name__ == '__main__':
     Test()
